dd635426aa45a6a4fed672646ddb6f01dc207e1c
Botirjon <thedavlativich@gmail.com>
2023-10-26 07:03:57 +0500

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Welcome to calculator!");
+
+Console.WriteLine("Enter the first number: ");
+var number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Select: *, /, +, -, % ");
+var pattern = Console.ReadLine();
+
+Console.WriteLine("Enter the second number: ");
+var number2 = Convert.ToInt32(Console.ReadLine());
+
+switch (pattern)
+{
+    case "*":
+        Console.WriteLine($"Result: {number1 * number2}");
+        break;
+    case "/":
+        Console.WriteLine($"Result: {number1 / number2}");
+        break;
+    case "+":
+        Console.WriteLine($"Result: {number1 + number2}");
+        break;
+    case "-":
+        Console.WriteLine($"Result: {number1 - number2}");
+        break;
+    case "%":
+        Console.WriteLine($"Result: {number1 % number2}");
+        break;
+}
